8828840f2c5dbaa50d097d95bb3187b60256c57c
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-11-11 13:38:56 -0500

    Update outputs.tf

--- intTerraform/outputs.tf
+++ intTerraform/outputs.tf
@@ -1,28 +1,28 @@
 
 output "ecs_cluster_id" {
-  value = data.aws_ecs_cluster.aws-ecs-cluster.id
+  value = aws_ecs_cluster.aws-ecs-cluster.id
 }
 
 output "vpc_id" {
-  value = data.aws_vpc.app_vpc.id
+  value = aws_vpc.app_vpc.id
 }
 
 output "subnet_ida" {
-  value = data.aws_subnet.public_a.id
+  value = aws_subnet.public_a.id
 }
 
 output "subnet_idb" {
-  value = data.aws_subnet.public_b.id
+  value = aws_subnet.public_b.id
 }
 
 output "alb_security_group_id" {
-  value = data.aws_security_group.http.id
+  value = aws_security_group.http.id
 }
 
 output "ingress_security_group_id" {
-  value = data.aws_security_group.ingress_app.id
+  value = aws_security_group.ingress_app.id
 }
 
 output "internet_gateway" {
-  value = data.aws_internet_gateway.igw.id
+  value = aws_internet_gateway.igw.id
 }
