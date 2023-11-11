f8ecfe15d19018cfd44a18a8efa047d70cc7a846
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-11-11 14:03:59 -0500

    Update ALB.tf

--- FrontendTF/ALB.tf
+++ FrontendTF/ALB.tf
@@ -4,7 +4,7 @@ resource "aws_lb_target_group" "ecom-app" {
   port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = aws_vpc.app_vpc.id                            #update with actuals ID
+  vpc_id      = "vpc-0275fe41113f0ca69"                            #update with actuals ID
 
   health_check {
     enabled = true
@@ -21,15 +21,15 @@ resource "aws_alb" "ecom_app" {
   load_balancer_type = "application"
 
   subnets = [
-    aws_subnet.public_a.id,                            #update with actuals ID
-    aws_subnet.public_b.id,                            #update with actuals ID
+    "subnet-0297bfe1d4f5960e0",                            #update with actuals ID
+    "subnet-0be4f13b9700414df",                            #update with actuals ID
   ]
 
   security_groups = [
-    aws_security_group.http.id,                            #update with actuals ID
+    "sg-0b91d06ecfe3579da",                            #update with actuals ID
   ]
 
-  depends_on = [aws_internet_gateway.igw]                            #update with actuals ID
+  depends_on = "igw-029226251b6b77124"                            #update with actuals ID
 }
 
 resource "aws_alb_listener" "ecom_app_listener" {
