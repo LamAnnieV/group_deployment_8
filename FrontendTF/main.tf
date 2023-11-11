689b96f3aaeab747eede95243b0e15c71ced3b36
Sameen Khan <128739962+Sameen-k@users.noreply.github.com>
2023-11-10 23:17:09 -0500

    Update main.tf

--- FrontendTF/main.tf
+++ FrontendTF/main.tf
@@ -5,6 +5,18 @@ provider "aws" {
 
 }
 
+data "aws_vpc" "D8-VPC" {
+  vpc_id = "vpc-07b9eeeceeed76834"
+}
+
+data "aws_subnet" "public_a" {
+  subnet_id = "subnet-01249b7ad6ecbca1b"
+}
+
+data "aws_subnet" "public_b" {
+  subnet_id = "subnet-00a5adc02b96b082f"
+}
+
 # Cluster
 data "aws_ecs_cluster" "existing_ecs_cluster" {
   cluster_name = "ecomapp-cluster"
