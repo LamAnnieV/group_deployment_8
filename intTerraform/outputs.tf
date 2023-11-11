5ffb54ac1ae7fc55d6efbc74d0b279f0d8361dda
Sameen Khan <128739962+Sameen-k@users.noreply.github.com>
2023-11-11 11:09:18 -0500

    Update outputs.tf

--- intTerraform/outputs.tf
+++ intTerraform/outputs.tf
@@ -21,3 +21,20 @@ output "alb_security_group_id" {
 output "internet_gateway" {
   value = aws_internet_gateway.NAME.id
 }
+
+data "aws_vpc" "D8-VPC" {
+  vpc_id = "vpc-07b9eeeceeed76834"
+}
+
+data "aws_subnet" "public_a" {
+  subnet_id = "subnet-01249b7ad6ecbca1b"
+}
+
+data "aws_subnet" "public_b" {
+  subnet_id = "subnet-00a5adc02b96b082f"
+}
+
+# Cluster
+data "aws_ecs_cluster" "existing_ecs_cluster" {
+  cluster_name = "ecomapp-cluster"
+}
