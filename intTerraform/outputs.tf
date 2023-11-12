184a35fc215f5dae5ad27bfbc54779f8a5a61480
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-11-12 10:00:30 -0500

    Update outputs.tf

--- intTerraform/outputs.tf
+++ intTerraform/outputs.tf
@@ -29,6 +29,7 @@ output "ingress_sg_FE_id" {
 
 output "aws_lb_target_group.ecom-app-FE.arn" {
   value = aws_lb_target_group.ecom-app-FE.arn
+}
 
 output "internet_gateway" {
   value = aws_internet_gateway.igw.id
